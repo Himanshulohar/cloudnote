06816cd6a8d421ae957cabba504a4cf19370d143
Himanshulohar <54778260+Himanshulohar@users.noreply.github.com>
2025-10-20 20:21:44 +0530

    Changed to port 5000

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-
+const { body, validationResult } = require('express-validator');
 // Create User using POST "/api/auth"
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  //res.send(req.body);
+router.get('/createuser', (req, res) => {
+
 });
 module.exports = router;
